4fc0a0a041a40c9358a18b43019fffc90737b353
Kevin_Gillispie <oakenquill@gmail.com>
2017-08-22 16:35:24 -0700

    init commit

---
+++ ChallengeDaysBetweenDates/BetweenTwoCalendars.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BetweenTwoCalendars.aspx.cs" Inherits="ChallengeDaysBetweenDates.BetweenTwoCalendars" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div><h3>How many days have elapsed?</h3></div>
+        <div><p>Chose the first date:<asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+            <br /></p></div>
+        <div><p>Chose the second date:<asp:Calendar ID="Calendar2" runat="server" OnSelectionChanged="Calendar2_SelectionChanged"></asp:Calendar>
+            </p></div>
+        <div><br />
+            <asp:Button ID="calcButton" runat="server" Text="Calculate" OnClick="calcButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeDaysBetweenDates/BetweenTwoCalendars.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class BetweenTwoCalendars : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Calendar2_SelectionChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void calcButton_Click(object sender, EventArgs e)
+        {
+            DateTime firstCal = Calendar1.SelectedDate;
+            DateTime secondCal = Calendar2.SelectedDate;
+
+            TimeSpan firstDay = DateTime.Now.Subtract(firstCal);
+            double day1 = firstDay.TotalDays;
+
+            TimeSpan secondDay = DateTime.Now.Subtract(secondCal);
+            double day2 = secondDay.TotalDays;
+
+            double numDays = day2 - day1;
+
+            if (numDays < 0)
+            {
+                numDays = (numDays * -1);
+            }
+
+            resultLabel.Text = numDays.ToString();
+        }
+    }
+}
